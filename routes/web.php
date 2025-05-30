0d3bb8a2deee8d65c7b5918a254fc18d9d06c45a
TANAY BORADE <96096852+Tanayb21@users.noreply.github.com>
2025-05-30 20:06:31 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,40 @@
-<?php
+// <?php
 
+// use Illuminate\Support\Facades\Route;
+
+// use App\Http\Controllers\FormController;
+// use App\Http\Controllers\SubscriberController;
+
+// Route::post('/subscribe', [SubscriberController::class, 'store'])->name('subscribe.store');
+
+
+// use App\Http\Controllers\ContactFormController;
+
+// Route::get('/contact', function () {
+//     return view('contact'); // Make sure to replace this with your contact form view
+// });
+
+// Route::post('/contact', [ContactFormController::class, 'store']);
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::view('/', 'welcome');
+// Route::view('/learnmore', 'learnmore');
+// Route::view('/contact', 'contactus');
+// Route::view('/abt', 'aboutus');
+// Route::view('/services', 'services');
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\SubscriberController;
-
-Route::post('/subscribe', [SubscriberController::class, 'store'])->name('subscribe.store');
-
-
 use App\Http\Controllers\ContactFormController;
 
-Route::get('/contact', function () {
-    return view('contact'); // Make sure to replace this with your contact form view
-});
-
+Route::post('/subscribe', [SubscriberController::class, 'store'])->name('subscribe.store');
 Route::post('/contact', [ContactFormController::class, 'store']);
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::view('/', 'welcome');
 Route::view('/learnmore', 'learnmore');
 Route::view('/contact', 'contactus');
-Route::view('/abt', 'aboutus');
+Route::view('/abt', 'aboutus');      // Ensure this file exists
 Route::view('/services', 'services');
